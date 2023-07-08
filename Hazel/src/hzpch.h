f52a88aad2c32d62c824c1e9c61de69d1e9a488f
SeeYouCowboi <teacatjazz@gmail.com>
2023-07-08 21:25:05 +0800

    增加了预编译头文件

---
+++ Hazel/src/hzpch.h
@@ -0,0 +1,18 @@
+#pragma once
+// Ô¤±àÒëÍ·ÎÄ¼þ
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <cstdio>
+
+#ifdef HZ_PLAYFORM_WINDOWS
+	#include <Windows.h>
+#endif // HZ_PLAYFORM_WINDOWS
